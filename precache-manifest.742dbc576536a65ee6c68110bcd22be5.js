4fea60a0e425e5a774b58df6c66a10c12533df2f
RomanLitsiiuk <alexandrovich2015@gmail.com>
2019-01-31 17:34:46 +0200

    Updates

---
+++ precache-manifest.742dbc576536a65ee6c68110bcd22be5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "df9e76fee848c24be44ea1040b270454",
+    "url": "/todo-react/static/media/noun_321395_cc.df9e76fe.svg"
+  },
+  {
+    "revision": "7107b870e115af8fd09e",
+    "url": "/todo-react/static/css/main.0cf702a5.chunk.css"
+  },
+  {
+    "revision": "42bbe4eefcde1297b11dc4b6491e9746",
+    "url": "/todo-react/static/media/Roboto-Italic.42bbe4ee.ttf"
+  },
+  {
+    "revision": "0c66215d61470a5bf3e3",
+    "url": "/todo-react/static/js/1.0c66215d.chunk.js"
+  },
+  {
+    "revision": "4ebad8e7a9f5566e1598",
+    "url": "/todo-react/static/js/runtime~main.4ebad8e7.js"
+  },
+  {
+    "revision": "d7a57fa9af95503795bc4d20ac4334e5",
+    "url": "/todo-react/static/media/map1920.d7a57fa9.png"
+  },
+  {
+    "revision": "eead282cc48fe7c3212d4ecfb372c6e1",
+    "url": "/todo-react/static/media/noun_321339_cc.eead282c.svg"
+  },
+  {
+    "revision": "33f36c412bea5fca876c13abfe252af3",
+    "url": "/todo-react/static/media/noun_321315_cc.33f36c41.svg"
+  },
+  {
+    "revision": "f129c40a320923d62e9deaba05a4311a",
+    "url": "/todo-react/static/media/noun_321363_cc.f129c40a.svg"
+  },
+  {
+    "revision": "d9ea99a3fb170df4845d6a2483b0bd02",
+    "url": "/todo-react/static/media/noun_321399_cc.d9ea99a3.svg"
+  },
+  {
+    "revision": "7107b870e115af8fd09e",
+    "url": "/todo-react/static/js/main.7107b870.chunk.js"
+  },
+  {
+    "revision": "9cef964b36116521163f44afdc7b28ca",
+    "url": "/todo-react/static/media/Sarabun-Regular.9cef964b.ttf"
+  },
+  {
+    "revision": "23237c2ccdc6e9ef7cddd0bf4078f589",
+    "url": "/todo-react/static/media/Sarabun-Italic.23237c2c.ttf"
+  },
+  {
+    "revision": "4f70ff7dd351fe841b15de5a88a499aa",
+    "url": "/todo-react/static/media/Sarabun-Bold.4f70ff7d.ttf"
+  },
+  {
+    "revision": "83ccca1df997a1919077b64c3d70ac30",
+    "url": "/todo-react/static/media/Sarabun-Light.83ccca1d.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/todo-react/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/todo-react/static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "/todo-react/static/media/Roboto-Light.fc84e998.ttf"
+  },
+  {
+    "revision": "0c66215d61470a5bf3e3",
+    "url": "/todo-react/static/css/1.52cecf24.chunk.css"
+  },
+  {
+    "revision": "451374ebdb7f1d12857018e96f5d3336",
+    "url": "/todo-react/index.html"
+  }
+];
